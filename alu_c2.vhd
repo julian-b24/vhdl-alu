41a9dfca2a535ce5b53039e40a49c24c25fd1a14
Julian Bolanos <julian.camilob24@gmail.com>
2022-04-21 14:46:57 -0500

    Alu completed

--- alu_c2.vhd
+++ alu_c2.vhd
@@ -12,6 +12,25 @@ end alu_c2;
 
 architecture behavior of alu_c2 is
     signal final : std_logic_vector(3 downto 0);
+
+    component sum_4 is
+        port(
+            a : in std_logic_vector(3 downto 0);
+            b : in std_logic_vector(3 downto 0);
+            cin: in std_logic;
+            sum : out std_logic_vector(3 downto 0);
+            cout : out std_logic
+        );
+    end component;
+
+    component substraction is
+        port(
+            a : in std_logic_vector(3 downto 0);
+            b : in std_logic_vector(3 downto 0);
+            result : out std_logic_vector(3 downto 0)
+        );
+    end component;
+
     begin
         process define_operation(a, b, operation) is
             begin
@@ -20,17 +39,25 @@ architecture behavior of alu_c2 is
                     when "000" =>
                         final <= "0000";
                     when "001" =>
-                        final <= "0001";
+                        final <= "1111";
                     when "010"
                         final_aux : substraction port map(a, b, final);
                     when "011"
                         final_aux : sum_4 port map(a, b, 0, final);
                     when "100"
-                        
-                
+                        final <= a and b;
+                    when "101"
+                        final <= not(a and b);
+                    when "110"
+                        final <= a or b;
+                    when "111"
+                        final <= not(a or b);
                     when others =>
+                        final <= not a;
                 
                 end case ;
         end process
+
+        result <= final;
 
 end behavior;
